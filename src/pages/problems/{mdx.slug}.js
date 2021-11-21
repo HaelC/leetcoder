0adbe863a9be5d7c91ec185dcd3f46fd99b3e64b
HaelChen <f.procumbens@gmail.com>
2021-11-21 14:36:33 -0600

    add topics and related problems

--- src/pages/problems/{mdx.slug}.js
+++ src/pages/problems/{mdx.slug}.js
@@ -1,16 +1,36 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../../components/layout";
 
+const RelatedProblems = ({ related_problems }) => {
+  if (related_problems.length === 0) {
+    return null;
+  }
+  return (
+    <div className="related-problems">
+      <h4>Related Problems:</h4>
+      <ul>
+        {related_problems.map(problem => (
+          <li key={problem}>
+            <Link to={`/problems/${problem[1]}`}>{problem[0]}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const ProblemPage = ({ data }) => {
-  const { title, difficulty, link, number } = data.mdx.frontmatter;
+  const { title, difficulty, link, number, related_problems, topics } =
+    data.mdx.frontmatter;
+  console.log(related_problems);
   return (
     <Layout pageTitle={title}>
-      <h2 className="problem-title">
-        {number}. {title}
-      </h2>
       <div className="problem-info">
+        <h2 className="problem-title">
+          {number}. {title}
+        </h2>
         <span className={`difficulty ${difficulty}`}>
           {difficulty.toUpperCase()}
         </span>
@@ -22,8 +42,17 @@ const ProblemPage = ({ data }) => {
         >
           LeetCode Link
         </a>
+        <ul className="problem-topics">
+          Topics:
+          {topics.map(topic => (
+            <li className="problem-topic" key={topic}>
+              {topic}
+            </li>
+          ))}
+        </ul>
       </div>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      <RelatedProblems related_problems={related_problems} />
     </Layout>
   );
 };
@@ -36,6 +65,8 @@ export const query = graphql`
         difficulty
         link
         number
+        related_problems
+        topics
       }
       body
     }
