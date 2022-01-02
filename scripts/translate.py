876c694f5604273b79c4e64f9bc361cbe62eab45
HaelChen <f.procumbens@gmail.com>
2022-01-02 11:27:59 -0600

    add-two-numbers solution and translate script

---
+++ scripts/translate.py
@@ -0,0 +1,53 @@
+import re
+
+filename = input("Mdx filename: ")
+solution = ""
+
+path_en = "../solutions/en/solution/"
+path_zh = "../solutions/zh/solution/"
+extension = ".mdx"
+full_filename = path_en + filename + extension
+new_filename = path_zh + filename + extension
+
+with open(full_filename) as f:
+    solution = f.read()
+
+difficulties = {'easy': '简单', 'medium': '中等', 'hard': '困难'}
+tags = {
+    'Array': '数组', 'String': '字符串', 'Hash Table': '哈希表', 
+    'Dynamic Programming': '动态规划', 'Two Pointers': '双指针',
+    'Sorting': '排序', 'Linked List': '链表', 'Math': '数学',
+    'Depth-First Search': '深度优先搜索', 'Binary Search': '二分查找',
+    'Breadth-First Search': '广度优先搜索', 'Greedy': '贪心',
+    'Tree': '树', 'Binary Tree': '二叉树', 'Stack': '栈',
+    'Queue': '队列', 'Heap (Priority Queue)': '堆（优先队列）',
+    'Recursion': '递归', 'Backtracking': '回溯'
+}
+word_dict = {
+    'Analysis:': '分析：', 'Time complexity:': '时间复杂度：',
+    'Space complexity:': '空间复杂度：', 'Follow-ups': '后续问题',
+    'Input:': '输入：', 'Output:': '输出：'
+}
+
+for difficulty_en, difficulty_zh in difficulties.items():
+    solution = solution.replace(difficulty_en, difficulty_zh)
+
+for tag_en, tag_zh in tags.items():
+    # solution = solution.replace(tag_en, tag_zh)
+    solution = re.sub(fr'tags: \[(.*?){tag_en}(.*?)\]',
+           fr'tags: [\1{tag_zh}\2]',
+           solution)
+
+for word_en, word_zh in word_dict.items():
+    solution = solution.replace(word_en, word_zh)
+
+# edit leetcode link
+solution = solution.replace('leetcode', 'leetcode-cn')
+# can't simply replace 'Solution' with '解法', because
+# there are 'Solution' in the code as well. 
+solution = solution.replace('### Solution', '### 解法')
+solution = solution.replace('C++ Solution', 'C++解法')
+solution = solution.replace('Java Solution', 'Java解法')
+
+with open(new_filename, 'w') as f:
+    f.write(solution)
